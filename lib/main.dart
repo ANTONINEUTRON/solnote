2d2123241fb429f3457dc6120151eebb4697bac1
Anthony Ameh <antonineutron@gmail.com>
2025-04-10 17:44:21 +0100

    project initiated

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:solnote/features/home/page/home_page.dart';
+import 'package:solnote/utils/app_constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConstants.appName,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static final appName = "Sol Note";
+  static final description =
+      "Never miss a move on Solana"; // Maling banter a rewarding experience
+
+  static final String baseUrl ="https://takesavenue-backend.vercel.app/api";
+  
+  
+}
